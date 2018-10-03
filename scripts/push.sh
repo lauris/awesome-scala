7471be0d0ce7013d1661b68aa82066e5de435004
Sakib Hadziavdic <sake.etf@gmail.com>
2018-10-03 22:51:40 +0200

    test travis

--- scripts/push.sh
+++ scripts/push.sh
@@ -10,8 +10,8 @@ echo "Set git config START"
 
 commit_files() {
  echo "commit START"
- git diff
-  git checkout master
+  git config core.filemode false # ignore chmod changes
+  #git checkout master
   git add README.md
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
   echo "commit END"
@@ -20,7 +20,8 @@ commit_files() {
 upload_files() {
  echo "upload_files START"
   git remote add origin-repo https://${GITHUB_TOKEN}@github.com/sake92/awesome-scala.git > /dev/null 2>&1
-  git push --quiet origin-repo master 
+  git push --quiet origin-repo test-travis 
+  #git push --quiet origin-repo master 
   echo "upload_files END"
 }
 
