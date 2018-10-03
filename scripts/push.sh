fb6de9e94ba6a97fff358a9b5abc561b86128af7
Sakib Hadziavdic <sake.etf@gmail.com>
2018-10-03 22:38:23 +0200

    test travis

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
 
 setup_git() {
-  git config --local user.email "travis@travis-ci.org"
-  git config --local user.name "Travis CI"
+echo "Set git config START"
+
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+ echo "Set git config END"
 }
 
 commit_files() {
+ echo "commit START"
   git checkout master
   git add README.md
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  echo "commit END"
 }
 
 upload_files() {
+ echo "upload_files START"
   git remote add origin-repo https://${GITHUB_TOKEN}@github.com/sake92/awesome-scala.git > /dev/null 2>&1
-  git push --quiet origin-repo master
+  git push --quiet origin-repo master 
+  echo "upload_files END"
 }
 
 setup_git
